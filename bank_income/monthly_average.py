fa9eb345526c09e4638733a0cf998b11b4d599d5
Akshit Annadi <aannadi@plaid.com>
2023-02-06 13:02:24 -0800

    done

--- bank_income/monthly_average.py
+++ bank_income/monthly_average.py
@@ -2,7 +2,12 @@ from typing import List
 from datetime import date, timedelta
 from plaid.model.credit_bank_income import CreditBankIncome
 from plaid.model.credit_bank_income_source import CreditBankIncomeSource
-from util import get_total_amount_for_date_range
+from util import (
+    get_active_sources,
+    get_total_amount_for_date_range,
+    source_monthly_avg_net_income,
+    get_gross_income_from_net_income,
+)
 
 
 CATEGORIES_TO_INCLUDE = [
@@ -16,7 +21,6 @@ CATEGORIES_TO_INCLUDE = [
     "RENTAL",
     "TAX_REFUND",
 ]
-AVG_DAYS_MONTH = 30.4167
 
 
 """
@@ -45,56 +49,58 @@ def monthly_average_net_income(
 ) -> float:
     # Get "active" streams. These are defined as streams that have at least one transaction
     # within the active_source_window
-    active_window_end_date = date.today()
-    active_window_start_date = active_window_end_date - timedelta(active_source_window)
-    active_sources = []
-
-    for bank_income_item in report.items:
-        for source in bank_income_item.bank_income_sources:
-            # If source has an excluded category, then ignore it
-            if str(source.income_category) not in categories_to_include:
-                continue
-            # Check if source has at least one transaction within the active window or
-            # is a TAX_REFUND
-            if (
-                source.end_date >= active_window_start_date
-                or str(source.income_category) == "TAX_REFUND"
-            ):
-                active_sources.append(source)
+    active_sources = get_active_sources(
+        report, active_source_window, categories_to_include
+    )
 
     # Next, iterate through active sources, calculate average monthly for that source and add it
     # it to the user's total monthly average income
-    monthly_average_amount = 0.0
-    avg_window_end_date = date.today()
-    avg_window_start_date = avg_window_end_date - timedelta(avg_calculation_window)
+    net_monthly_average = 0.0
     for source in active_sources:
-        # For tax refund, amortize the amount evenly over the year
-        if str(source.income_category) == "TAX_REFUND":
-            monthly_average_amount += source.total_amount / 12
-            continue
+        net_monthly_average += source_monthly_avg_net_income(
+            source, active_source_window, avg_calculation_window
+        )
+    return net_monthly_average
 
-        if source.start_date >= active_window_start_date:
-            # For new jobs that started within the active window, average amount over the active window to get a
-            # daily average and multiply by the AVG_DAYS_MONTH multiple
-            monthly_average_amount += (
-                source.total_amount / active_source_window * AVG_DAYS_MONTH
-            )
-        elif source.start_date >= avg_window_start_date:
-            # For jobs that started within the averaging window, average amount since the start date to get a daily average
-            # and multiply by the AVG_DAYS_MONTH multiple
-            monthly_average_amount += (
-                source.total_amount
-                / (avg_window_end_date - source.start_date).days
-                * AVG_DAYS_MONTH
-            )
-        else:
-            # For jobs that have been ongoing, average amount over the avg_calculation_window to get a daily average
-            # and multiply by the AVG_DAYS_MONTH multiple
-            amount_in_avg_window = get_total_amount_for_date_range(
-                source, avg_window_start_date, avg_window_end_date
-            )
-            monthly_average_amount += (
-                amount_in_avg_window / avg_calculation_window * AVG_DAYS_MONTH
-            )
 
-    return monthly_average_amount
+"""
+monthly_average_gross_income takes in a Bank Income report and estimates the average monthly
+gross income for that user across their income sources
+Params
+* report: The Bank Income report to calculate monthly income for.
+* active_source_window: The time period, in days, that is used to determine whether an income source is active.
+    Only income sources that have recieved a transaction within this number of days will be included in the calculation.
+    Default, recommended value is 30 days.
+* avg_calculation_window: The time period, in days, over which to calculate the monthly average. We will average over
+    a maximum of this many days. Default, recommended value is 90 days.
+* categories_to_include: A list of income categories to include in the calculation. Only income sources that fall within one
+    of these categories will be considered.
+
+Returns
+* An average monthly gross income value across the avg_calculation_window
+"""
+
+
+def monthly_average_gross_income(
+    report: CreditBankIncome,
+    active_source_window: int = 30,
+    avg_calculation_window: int = 90,
+    categories_to_include: List[str] = CATEGORIES_TO_INCLUDE,
+) -> float:
+    # Get "active" streams. These are defined as streams that have at least one transaction
+    # within the active_source_window
+    active_sources = get_active_sources(
+        report, active_source_window, categories_to_include
+    )
+
+    # Next, iterate through active sources, calculate average gross monthly for that source and add it
+    # it to the user's total monthly average gross income
+    gross_monthly_average = 0.0
+    for source in active_sources:
+        monthly_average_net_amount = source_monthly_avg_net_income(
+            source, active_source_window, avg_calculation_window
+        )
+        gross_monthly_average += get_gross_income_from_net_income(
+            monthly_average_net_amount, str(source.income_category)
+        )
+    return gross_monthly_average
--- bank_income/util.py
+++ bank_income/util.py
@@ -1,3 +1,4 @@
+from typing import List
 from datetime import date, timedelta
 from plaid.model.credit_bank_income import CreditBankIncome
 from plaid.model.credit_bank_income_source import CreditBankIncomeSource
@@ -21,3 +22,94 @@ def get_total_amount_for_date_range(
             if transaction.date >= start_date and transaction.date <= end_date:
                 total_amount += transaction.amount
     return total_amount
+
+
+"""
+get_total_amount_for_date_range returns the "active" income sources
+that should be used in the monthly average income calculation
+"""
+
+
+def get_active_sources(
+    report: CreditBankIncome,
+    active_source_window: int,
+    categories_to_include: List[str],
+) -> List[CreditBankIncomeSource]:
+    active_window_end_date = date.today()
+    active_window_start_date = active_window_end_date - timedelta(active_source_window)
+    active_sources = []
+
+    for bank_income_item in report.items:
+        for source in bank_income_item.bank_income_sources:
+            # If source has an excluded category, then ignore it
+            if str(source.income_category) not in categories_to_include:
+                continue
+            # Check if source has at least one transaction within the active window or
+            # is a TAX_REFUND
+            if (
+                source.end_date >= active_window_start_date
+                or str(source.income_category) == "TAX_REFUND"
+            ):
+                active_sources.append(source)
+    return active_sources
+
+
+"""
+source_monthly_avg_net_income returns the monthly average net income
+for an income source across a given averaging window
+"""
+
+
+def source_monthly_avg_net_income(
+    source: CreditBankIncomeSource,
+    active_source_window: int,
+    avg_calculation_window: int,
+    AVG_DAYS_MONTH=30.4167,
+) -> float:
+    active_window_end_date = date.today()
+    active_window_start_date = active_window_end_date - timedelta(active_source_window)
+
+    avg_window_end_date = date.today()
+    avg_window_start_date = avg_window_end_date - timedelta(avg_calculation_window)
+
+    if str(source.income_category) == "TAX_REFUND":
+        # For tax refund, amortize the amount evenly over the year
+        return source.total_amount / 12
+    elif source.start_date >= active_window_start_date:
+        # For new jobs that started within the active window, average amount over the active window to get a
+        # daily average and multiply by the AVG_DAYS_MONTH multiple
+        return source.total_amount / active_source_window * AVG_DAYS_MONTH
+    elif source.start_date >= avg_window_start_date:
+        # For jobs that started within the averaging window, average amount since the start date to get a daily average
+        # and multiply by the AVG_DAYS_MONTH multiple
+        return (
+            source.total_amount
+            / (avg_window_end_date - source.start_date).days
+            * AVG_DAYS_MONTH
+        )
+    else:
+        # For jobs that have been ongoing, average amount over the avg_calculation_window to get a daily average
+        # and multiply by the AVG_DAYS_MONTH multiple
+        amount_in_avg_window = get_total_amount_for_date_range(
+            source, avg_window_start_date, avg_window_end_date
+        )
+        return amount_in_avg_window / avg_calculation_window * AVG_DAYS_MONTH
+
+
+"""
+get_gross_income_from_net_income estimates gross income from a net income
+value for a given category
+"""
+
+
+def get_gross_income_from_net_income(
+    amount: float,
+    category: str,
+) -> float:
+    # If the income is from salary, multiply by a 1.25x multiplier to account for taxes.
+    # Institutions like FreddieMac recommend multiplying by 1.25, which represents a general 25% tax
+    # rate in the United States and is typically the standard used by government-sponsored enterprises.
+    if category == "SALARY":
+        return amount * 1.25
+    # Otherwise return the net income as gross
+    return amount
